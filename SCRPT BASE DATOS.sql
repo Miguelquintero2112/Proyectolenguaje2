062f0d47f911da8d744fa96f4a684fd340e65cc8
Miguel Angelquintero Cortes <miguelquintero21122004@gmail.com>
2024-12-17 12:00:35 -0500

    Add files via upload

---
+++ SCRPT BASE DATOS.sql
@@ -0,0 +1,14 @@
+-- Database: cine_registro
+
+-- DROP DATABASE IF EXISTS cine_registro;
+
+CREATE DATABASE cine_registro
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Colombia.1252'
+    LC_CTYPE = 'Spanish_Colombia.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
